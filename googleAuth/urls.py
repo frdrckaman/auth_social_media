c80c666336a7c87d8a7e73f6185cd1e88399517e
frdrck <frdrckaman@gmail.com>
2020-05-05 13:34:53 +0300

    template modification

--- googleAuth/urls.py
+++ googleAuth/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('g_auth.urls'))
+    path('auth', include('g_auth.urls'))
 ]
